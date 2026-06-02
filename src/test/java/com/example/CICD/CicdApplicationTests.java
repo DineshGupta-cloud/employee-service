3b0a92857826876cad75a80ab6cd8db7627c61d3
dineshg009 <dineshg009@gmail.com>
2026-06-02 14:49:31 +0530

    change in ontroller

--- src/test/java/com/example/CICD/CicdApplicationTests.java
+++ src/test/java/com/example/CICD/CicdApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.CICD;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(5, 2 + 3);
 	}
 
 }
